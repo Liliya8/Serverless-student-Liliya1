8678757da9e7c2d835a218867f750e8970216382
Liliya Salikhova <lily@liliyas-mbp.local>
2021-06-27 23:56:47 +0300

    passwordcheck

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -2,9 +2,12 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var password = req.query.password
-
+    var your_response = "Access denied."
+    if (password == "letmein") {
+        your_response = "Access granted."
+    }
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: your_response
     };
 }
