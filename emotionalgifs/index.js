8d8a03152527b0ddefc24d12228853d643bf8d63
Liliya Salikhova <lily@liliyas-mbp.local>
2021-07-02 22:25:29 +0300

    updated my emotionalgifs function to send image to faceapi and send emotion results in response

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -1,25 +1,49 @@
 
 var multipart = require('parse-multipart');
+var fetch = require('node-fetch');
 
 
 
 module.exports = async function (context, req) {
 
 
-    // here's your boundary:
     var boundary = multipart.getBoundary(req.headers['content-type']);
 
-    // TODO: assign the body variable the correct value
     var body = req.body
 
-    // parse the body
-    var parts = multipart.Parse(body, boundary);
-    var convertedResult = Buffer.from(parts[0].data).toString('base64');
-    // FILL IN THE BLANK
 
+    var parts = multipart.Parse(body, boundary);
+    var image = parts[0].data
+
+    var result = await analyzeImage(image)
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: convertedResult
+        body: { result }
     };
+}
+
+async function analyzeImage(img) {
+    //Change values while testing
+    const subscriptionKey = process.env.FACEAPI_KEY11;
+    const uriBase = process.env.FACEAPI_ENDPOINTT + '/face/v1.0/detect';
+
+    let params = new URLSearchParams({
+        'returnFaceId': 'true',
+        'returnFaceAttributes': 'emotion'     //FILL IN THIS LINE
+    })
+
+    let resp = await fetch(uriBase + '?' + params.toString(), {
+        method: 'POST',
+        body: img,
+
+
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    })
+    let data = await resp.json()
+    return data;
+
 }
