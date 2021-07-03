2c55c446ef2b072dd66dd30ccf6a2195d4d656cb
Liliya Salikhova <lily@liliyas-mbp.local>
2021-07-04 02:48:40 +0300

    modified Azure Function so that it texts the user back with a song

--- song4u/index.js
+++ song4u/index.js
@@ -21,8 +21,16 @@ module.exports = async function (context, req) {
     context.log(age);
     let generation = '';
 
+    const songs = {
+        "GenZ": "https://open.spotify.com/track/0SIAFU49FFHwR3QnT5Jx0k?si=1c12067c9f2b4fbf",
+        "GenY": "https://open.spotify.com/track/1Je1IMUlBXcx1Fz0WE7oPT?si=a04bbdf6ec4948b9",
+        "GenX": "https://open.spotify.com/track/4Zau4QvgyxWiWQ5KQrwL43?si=790d9e3ef2ed408d",
+        "BabyBoomers": "https://open.spotify.com/track/4gphxUgq0JSFv2BCLhNDiE?si=1abb329f2dc24f50",
+        "Unknown": "https://open.spotify.com/track/5ygDXis42ncn6kYG14lEVG?si=84b49b41d09d4d11"
+    }
+
     if (age > 5 && age < 25) {
-        generation = 'Genz';
+        generation = 'GenZ';
     } else if (age > 24 && age < 41) {
         generation = 'GenY';
     } else if (age > 40 && age < 57) {
@@ -33,16 +41,21 @@ module.exports = async function (context, req) {
         generation = 'unknown';
     }
     context.log(generation);
+
+    const url = songs[generation]
+
+    const message = `We guessed you're part of this generation: ${generation}! Happy listening! ${url}`
+
     context.res = {
         //status:200, /* Defaults to 200 */
-        body: generation
+        body: message
     };
 }
 async function analyzeImage(image) {
     //subscription key and endpoint
     //const suscriptKey=process.env.SUBSCRIPTION_KEY;
     //const theUrl=process.env.SONGREC_ENDPOINT+'face/v1.0/detect';
-    const subscriptKey = process.env.FACEAPI_KEY11;
+    const subscriptKey = process.env.FACEAPI_ENDPOINTT;
     const theUrl = process.env.FACEAPI_ENDPOINTT + 'face/v1.0/detect';
 
     let theParameters = new URLSearchParams({
