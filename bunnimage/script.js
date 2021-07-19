54170f8305c8210451e0b594c4104ffb292a6a3c
Liliya Salikhova <lily@liliyas-mbp.local>
2021-07-19 09:59:37 +0300

    code fixed

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,8 +1,42 @@
-function getImage(event) {
+async function getImage(event) {
     event.preventDefault()
-    if (document.getElementById('name').value != '') {
-        $('#output').text("Thanks!")
+    var myform = document.getElementById("myform")
+    // get image and filename uploaded by user via the form 
+    let nameInput = document.getElementById("name");
+    let fileInput = document.getElementById("image");
+    let file = fileInput.files[0];
+
+    var payload = new FormData(myform);
+    payload.append("file", file);
+    $('#output').text("Thanks!")
+
+    if (document.getElementById("name").value !== '') {
+
+
+        try {
+            let url = "https://func-liliya-hackervoice.azurewebsites.net/api/bunnimage-upload?"
+            console.log("Image was uploaded, making POST request to Azure function")
+
+            // create request to Azure function
+            const resp = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'codename': nameInput.value
+                },
+                body: payload
+            })
+
+
+            console.log("resp", resp)
+
+            $('#output').text("Your image has been stored successfully!")
+        } catch (err) {
+            $('#output').text(err)
+        }
+
     } else {
         alert("No name error.")
     }
+
+
 }
