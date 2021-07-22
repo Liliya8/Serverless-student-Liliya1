6a609c4eec9c7eba4932ff50b270277c1f146903
Liliya Salikhova <lily@liliyas-mbp.local>
2021-07-22 18:38:08 +0300

    get cat pictures

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,17 +1,30 @@
 function y1k3s() {
+    let name1 = document.getElementById("name1").value
 
-    const imgInput = document.getElementById("imageInput");
-    if (imgInput.value !== "") {
-
-        fetch(`https://cataas.com/cat/${imgInput.value}?json=true`)
-            .then(res => res.json())
-            .then(resp => {
-                console.log("response", resp)
-                const image = document.getElementById("image");
-                image.src = `https://cataas.com${resp.url}`;
-            })
-    } else {
-        alert("no input error")
+    let endpoint1 = "https://cataas.com/cat/cute/says/" + name1
+    if (name1 != '') {
+        document.getElementById("image1").src = endpoint1//call to API
     }
 
+    let name2 = document.getElementById("name2").value
+
+    let endpoint2 = "https://cataas.com/cat/cute/says/" + name2
+    if (name2 != '') {
+        document.getElementById("image2").src = endpoint2//call to API
+    }
+
+    let name3 = document.getElementById("name3").value
+
+    let endpoint3 = "https://cataas.com/cat/cute/says/" + name3
+    if (name3 != '') {
+        document.getElementById("image3").src = endpoint3//call to API
+    }
+
+
+    let name4 = document.getElementById("name4").value
+
+    let endpoint4 = "https://cataas.com/cat/cute/says/" + name4
+    if (name4 != '') {
+        document.getElementById("image4").src = endpoint4//call to API
+    }
 }
