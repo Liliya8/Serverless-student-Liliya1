2e45063534f6c5bf1b38a48005187d2c3cd59ab3
Liliya Salikhova <lily@liliyas-mbp.local>
2021-07-23 01:59:24 +0300

    test fix

--- catzapp/index.js
+++ catzapp/index.js
@@ -1,23 +1,48 @@
 function y1k3s() {
+    const toDataURL = url => fetch(url)
+        .then(response => response.blob())
+        .then(blob => new Promise((resolve, reject) => {
+            const reader = new FileReader()
+            reader.onloadend = () => resolve(reader.result)
+            reader.onerror = reject
+            reader.readAsDataURL(blob)
+        }))
+
     let name1 = document.getElementById("name1").value
 
     let endpoint1 = "https://cataas.com/cat/cute/says/" + name1
     if (name1 != '') {
-        document.getElementById("image1").src = endpoint1//call to API
+        toDataURL(endpoint1)
+            .then(dataUrl => {
+                dataUrl = dataUrl.split(",")
+                dataUrl[0] = "data:image/png;base64";
+                document.getElementById("image1").src = dataUrl;   //call to API
+            })
+
     }
 
     let name2 = document.getElementById("name2").value
 
     let endpoint2 = "https://cataas.com/cat/cute/says/" + name2
     if (name2 != '') {
-        document.getElementById("image2").src = endpoint2//call to API
+        toDataURL(endpoint2)
+            .then(dataUrl => {
+                dataUrl = dataUrl.split(",")
+                dataUrl[0] = "data:image/png;base64";
+                document.getElementById("image2").src = dataUrl;   //call to API
+            })
     }
 
     let name3 = document.getElementById("name3").value
 
     let endpoint3 = "https://cataas.com/cat/cute/says/" + name3
     if (name3 != '') {
-        document.getElementById("image3").src = endpoint3//call to API
+        toDataURL(endpoint3)
+            .then(dataUrl => {
+                dataUrl = dataUrl.split(",")
+                dataUrl[0] = "data:image/png;base64";
+                document.getElementById("image3").src = dataUrl;   //call to API
+            })
     }
 
 
@@ -25,6 +50,11 @@ function y1k3s() {
 
     let endpoint4 = "https://cataas.com/cat/cute/says/" + name4
     if (name4 != '') {
-        document.getElementById("image4").src = endpoint4//call to API
+        toDataURL(endpoint4)
+            .then(dataUrl => {
+                dataUrl = dataUrl.split(",")
+                dataUrl[0] = "data:image/png;base64";
+                document.getElementById("image4").src = dataUrl;   //call to API
+            })
     }
 }
